f77e173bd176e3a94ae132e783c7b3c9c8d96548
Jakub Jurga <>
2021-03-04 12:59:50 +0000

    Add js game results and comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Odczytanie ruchu komputera
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -14,6 +16,7 @@ if(randomNumber == 1){
 
 printMessage('Mój ruch to: ' + computerMove);
 
+//Odczytanie ruchu gracza
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
@@ -30,3 +33,53 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+// Wynik gry
+
+if (computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+
+else if (computerMove == 'papier' && playerMove == 'papier'){
+	printMessage('Remis!');
+}
+
+else if (computerMove == 'nożyce' && playerMove == 'papier'){
+	printMessage('Komputer wygrywa!');
+}
+
+else if (computerMove == 'kamień' && playerMove == 'kamien'){
+  printMessage('Remis!');
+}
+
+else if (computerMove == 'papier' && playerMove == 'kamien'){
+  printMessage('Komputer wygrywa!');
+}
+
+else if (computerMove == 'nożyce' && playerMove == 'kamien'){
+  printMessage('Ty wygrywasz!');
+}
+
+else if (computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Komputer wygrywa!');
+}
+
+else if (computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+}
+
+else if (computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis!');
+}
+
+else if (computerMove == 'kamien' && playerMove == 'nieznany ruch'){
+  printMessage('Błąd! Gra nie może się rozpocząć!');
+}
+
+else if (computerMove == 'papier' && playerMove == 'nieznany ruch'){
+  printMessage('Błąd! Gra nie może się rozpocząć!');
+}
+
+else if (computerMove == 'nożyce' && playerMove == 'nieznany ruch'){
+  printMessage('Błąd! Gra nie może się rozpocząć!');
+}
